a28933da5567c70887eb6f6169ebf849726d7b08
Manuel Pichler <mapi@manuel-pichler.de>
2010-01-22 20:41:08 +0100

    # Now binary detection also works on windows installations.

--- src/de/whitewashing/php/cs/CodeSnifferBinary.java
+++ src/de/whitewashing/php/cs/CodeSnifferBinary.java
@@ -6,7 +6,6 @@
 package de.whitewashing.php.cs;
 
 import java.io.File;
-import java.io.FilePermission;
 
 /**
  *
@@ -21,6 +20,13 @@ public class CodeSnifferBinary {
     private String pathName = null;
 
     private boolean initialized = false;
+
+    /**
+     * List of possible cli tool extensions. Note that the order of the
+     * extensions is important, otherwise the cli tool detection coould fail
+     * on windows.
+     */
+    private String[] extensions = {".bat", ".php"};
 
     public CodeSnifferBinary()
     {
@@ -49,22 +55,60 @@ public class CodeSnifferBinary {
         }
         this.initialized = true;
 
-        String env = System.getenv("PATH");
-        if (env == null || env.trim().equals("")) {
-            return null;
-        }
-
-        String[] paths = env.split(File.pathSeparator);
-        for (String path: paths) {
-            File f = new File(path + File.separator + this.fileName);
-            if (!f.exists() || !checkExec(f.getAbsolutePath())) {
-                continue;
+        for (String path: this.getPossiblePaths()) {
+            if (this.isExecutable(path + File.separator + this.fileName)) {
+                return this.pathName;
             }
-
-            this.pathName = f.getAbsoluteFile().getPath();
-            break;
         }
         return this.pathName;
+    }
+
+    /**
+     * Returns an array with all possible system paths where the CodeSniffer
+     * binary could exist.
+     *
+     * @return String[]
+     */
+    private String[] getPossiblePaths() {
+        String env = System.getenv("PATH");
+        if (env == null || env.trim().equals("")) {
+            return new String[0];
+        }
+        return env.split(File.pathSeparator);
+    }
+
+    /**
+     * Tests if the CodeSniffer executable exists for various operation system.
+     *
+     * @param path The raw executable path without system dependent extension.
+     *
+     * @return boolean
+     */
+    private boolean isExecutable(String path) {
+        for (String extension : this.extensions) {
+            if (this.isExecutable(path, extension)) {
+                return true;
+            }
+        }
+        return this.isExecutable(path, "");
+    }
+
+    /**
+     * Checks if the given path + extension is an existent and executable
+     * CodeSniffer binary.
+     *
+     * @param path The raw executable path without system dependent extension.
+     * @param extension A possible extension of the CodeSniffer executable.
+     *
+     * @return boolean
+     */
+    private boolean isExecutable(String path, String extension) {
+        File f = new File(path + extension);
+        if (f.exists() && checkExec(f.getAbsolutePath())) {
+            this.pathName = f.getAbsoluteFile().getPath();
+            return true;
+        }
+        return false;
     }
 
     private boolean checkExec(String path) {
